33258e2a3035ccad363cd33b14025ef05b8bc26a
newmarch2026 <gcpmake17@gmail.com>
2026-03-21 10:00:04 +0600

    Create index.php

---
+++ 20-3-2026/new-heldoly/wordpress/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Front to the WordPress application.
+ * Directed exclusively to the Hello Dolly plugin.
+ *
+ * @package WordPress
+ */
+
+// Define WordPress theme constant (though likely unused without core)
+define('WP_USE_THEMES', true);
+
+// Load the Hello Dolly plugin directly
+require_once __DIR__ . '/wordpress/wp-content/plugins/hello-dolly/hello.php';
